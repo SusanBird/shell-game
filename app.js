4abf2e39b35623c09d258ccc7348f5e02d240302
SusanBird <susanbird391@gmail.com>
2022-03-08 11:39:27 -0800

    grabbed DOM elements

--- app.js
+++ app.js
@@ -1,4 +1,12 @@
 // import functions and grab DOM elements
+const firstButton = document.getElementById('first-cup-button');
+const secondButton = document.getElementById('second-cup-button');
+const thirdButton = document.getElementById('third-cup-button');
+const imgCupNoBall = document.getElementById('cup-no-ball-img');
+const imgCupWithBall = document.getElementById('cup-with-ball-img');
+const winsEl = document.getElementById('wins');
+const lossesEl = document.getElementById('losses');
+const totalEl = document.getElementById('total');
 
 // let state
 
