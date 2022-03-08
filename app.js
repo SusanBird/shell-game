a2035cb2d47130d6098ed641d56171155f7ed7d9
SusanBird <susanbird391@gmail.com>
2022-03-08 11:57:34 -0800

    wrote displayWinsLossesAndTotal function

--- app.js
+++ app.js
@@ -10,6 +10,101 @@ const totalEl = document.getElementById('total');
 
 // let state
 
+let wins = 0;
+let total = 0;
+
+function resetImages() {
+  // - (reset image of all cups)
+  // make each image be cup-no-ball
+    imgCupNoBall.src = './assets/cup-no-ball.png';
+}
+
+function displayWinsLossesAndTotal() {
+  // - Update DOM
+  //   - Change numbers in winEl, lossEl, and totalEl
+    winsEl.textContent = wins;
+    lossesEl.textContent = total - wins;
+    totalEl.textContent = total;
+}
+
+buttonOne.addEventListener('click', () => {
+    resetImages();  //not sure how this will work, better to just input the image instead of a function?
+    total++;
+
+    //decide which cup, at random 
+    const randomCupNumber = Math.floor(Math.random() * 3);
+
+    if (randomCupNumber === 0) {
+        wins++;
+
+        imgOne.src = './assets/correct-cup.png';
+
+    } else if (randomCupNumber === 1) {
+        imgTwo.src = './assets/correct-cup.png';
+
+    } else {
+        imgThree.src = './assets/correct-cup.png';
+    }
+
+    displayWinsLossesAndTotal();
+});
+
+
+buttonTwo.addEventListener('click', () => {
+    resetImages();
+// - Set State
+//   - Increment total attempts
+    total++;
+
+//   - Use Math.random() to decide which cup is correct
+    const randomCupNumber = Math.floor(Math.random() * 3);
+  // we end up with 0, 1, or 2
+
+    if (randomCupNumber === 0) {
+        imgOne.src = './assets/correct-cup.png';
+
+    } else if (randomCupNumber === 1) {
+        imgTwo.src = './assets/correct-cup.png';
+
+        // because they clicked on the second cup, and it turns out the second cup is the winner, increment the wins
+        wins++;
+    } else {
+        imgThree.src = './assets/correct-cup.png';
+    }
+
+    displayWinsLossesAndTotal();
+});
+
+buttonThree.addEventListener('click', () => {
+    resetImages();
+
+// - Set State
+//   - Increment total attempts
+    total++;
+
+//   - Use Math.random() to decide which cup is correct
+    const randomCupNumber = Math.floor(Math.random() * 3);
+  // we end up with 0, 1, or 2
+
+    if (randomCupNumber === 0) {
+        imgOne.src = './assets/correct-cup.png';
+
+    } else if (randomCupNumber === 1) {
+        imgTwo.src = './assets/correct-cup.png';
+
+    } else {
+      // because they clicked on the first cup, and it turns out the first cup is the winner, increment the wins
+        wins++;
+
+        imgThree.src = './assets/correct-cup.png';
+    }
+
+    displayWinsLossesAndTotal();
+});
+
+
+
+
 // set event listeners 
   // get user input
   // use user input to update state 
