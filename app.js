43bc1ccc31964f8531b2322868fa30bf3736f6b9
SusanBird <susanbird391@gmail.com>
2022-03-08 21:43:34 -0800

    restructuring functions

--- app.js
+++ app.js
@@ -3,13 +3,9 @@ const firstButton = document.getElementById('first-cup-button');
 const secondButton = document.getElementById('second-cup-button');
 const thirdButton = document.getElementById('third-cup-button');
 
-
 const firstCupImage = document.getElementById('first-cup-no-ball-img');
 const secondCupImage = document.getElementById('second-cup-no-ball-img');
 const thirdCupImage = document.getElementById('third-cup-no-ball-img');
-
-//const imgCupNoBall = document.getElementById('cup-no-ball-img');
-//const imgCupWithBall = document.getElementById('cup-with-ball-img');
 
 const winsEl = document.getElementById('wins');
 const lossesEl = document.getElementById('losses');
@@ -22,43 +18,59 @@ let total = 0;
 
 //console.log(wins, totals);
 
-//function resetImages() {
-  // - (reset image of all cups)
-  // make each image be cup-no-ball
-    //imgCupNoBall.src = './assets/cup-no-ball.png';
-//}
+// set event listeners 
+  // get user input
+  // use user input to update state 
+  // update DOM to reflect the new state
+firstButton.addEventListener('click', () => {
+    handleGuess(0);
+});
 
-function displayWinsLossesAndTotal() {
-  // - Update DOM
-  //   - Change numbers in winEl, lossEl, and totalEl
-    winsEl.textContent = wins;
-    lossesEl.textContent = total - wins;
-    totalEl.textContent = total;
-}
+secondButton.addEventListener('click', () => {
+    handleGuess(1);
+});
 
-function getRandomHidingSpot() {   //use this in handleGuess?
-    const hidingPlaces = [
-        'cup-one',
-        'cup-two',
-        'cup-three',
-    ];   
+thirdButton.addEventListener('click', () => {
+    handleGuess(2);
+});
 
-//decide which cup, at random 
-    const index = Math.floor(Math.random() * hidingPlaces.length);
-    return hidingPlaces[index];
-}
+// create function 
 
 function handleGuess(userGuess, correctSpot) {       //userGuess, correctSpot //define correctSpot?
-    
     firstCupImage.src = './assets/cup-no-ball.png';
     secondCupImage.src = './assets/cup-no-ball.png';
     thirdCupImage.src = './assets/cup-no-ball.png';
-   // resetImages();  //not sure how this will work, better to just input the image instead of a function?
     
     total++;
 
-    const hidingSpotEl = document.getElementById(correctSpot + '-container');
-    hidingSpotEl.src = './assets/cup-with-ball.png';
+    //decide which cup, at random 
+    function getRandomHidingSpot() {   //use this in handleGuess?
+        const hidingPlaces = [
+            'cup-one',
+            'cup-two',
+            'cup-three',
+        ]; 
+        const index = Math.floor(Math.random() * hidingPlaces.length);
+        return hidingPlaces[index];
+    }
+    
+    if (index === userGuess) {     //replace correctSpot with hidingSpotEl? No
+        wins++;
+    } 
+    function displayWinsLossesAndTotal() {
+      // - Update DOM
+      //   - Change numbers in winEl, lossEl, and totalEl
+        winsEl.textContent = wins;
+        lossesEl.textContent = total - wins;
+        totalEl.textContent = total;
+    } 
+    displayWinsLossesAndTotal();
+}
+
+
+
+//const hidingSpotEl = document.getElementById(correctSpot + '-container');
+    //hidingSpotEl.src = './assets/cup-with-ball.png';
 
 // then show image with ball
     //hidingSpotEl.classList.add(imgCupWithBall.src = './assets/cup-with-ball.png');     ///more complex code needed here to make this appear
@@ -74,33 +86,16 @@ function handleGuess(userGuess, correctSpot) {       //userGuess, correctSpot //
    // } else {
        // imgCupWithBall.src = './assets/cup-with-ball.png';
    // }
-    if (index === userGuess) {     //replace correctSpot with hidingSpotEl? No
-        wins++;
-    } 
 
-    displayWinsLossesAndTotal();
-}
-
-// set event listeners 
-  // get user input
-  // use user input to update state 
-  // update DOM to reflect the new state
-
-firstButton.addEventListener('click', () => {
-    handleGuess();
-});
-
-
-secondButton.addEventListener('click', () => {
-    handleGuess();
-});
-
-thirdButton.addEventListener('click', () => {
-    handleGuess();
-});
-
-
-
-//'cup-one', getRandomHidingSpot()
+   //'cup-one', getRandomHidingSpot()
 //'cup-two', getRandomHidingSpot()
 //'cup-three', getRandomHidingSpot()
+
+//function resetImages() {
+  // - (reset image of all cups)
+  // make each image be cup-no-ball
+    //imgCupNoBall.src = './assets/cup-no-ball.png';
+//}
+
+//const imgCupNoBall = document.getElementById('cup-no-ball-img');
+//const imgCupWithBall = document.getElementById('cup-with-ball-img');
